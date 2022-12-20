a88994067693bb68f1c5cd53d7989fd561e2dded
Mahmoud Galal <mahmoudgalal69@gmail.com>
2022-12-20 10:12:57 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <p>Copyright &copy; 2021</p>
+    <router-link v-show="about" to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  computed:{ 
+  about(){ 
+      if (this.$route.path == "/") {
+        return true
+      }
+      else { 
+        return false
+      }
+  }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #333;
+}
+
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
